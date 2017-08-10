4add724a340b9b57acd4db4a25e2501e34502981
c faust macbook pro <christina.faust@gmail.com>
2017-08-10 18:04:02 +0100

    fixing between species transmisison rates.

--- sensitivity/test_corematrixmodel_func.R
+++ sensitivity/test_corematrixmodel_func.R
@@ -1,33 +1,33 @@
-patch.matrix.model <- function(Time, State, Parameters) {
+core.matrix.model <- function(Time, State, Parameters) {
   with(as.list(c(State, Parameters)), {
-    N.p<-S.p+I.p+R.p
+    N.c<-S.c+I.c+R.c
     N.m<-S.m+I.m+R.m
     
-    dS.p<-N.p*(rmax.p+d.p)*(1-N.p/(k.p*(1.01-phi))) - ((S.p*beta.pp*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*I.m*S.p)/(N.p+epsilon*N.m)^kappa) - d.p*S.p 
-    dI.p<-(beta.pp*S.p*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*S.p*I.m)/(N.p+epsilon*N.m)^kappa - I.p*(alpha.p*d.p+d.p+gamma.p)
-    dR.p<-I.p*gamma.p-R.p*d.p
+    dS.c<-N.c*(rmax.c+d.c)*(1-N.c/(k.c*(1.01-phi))) - ((S.c*beta.c*I.c)/N.c^kappa+(epsilon*beta.mp*beta.cp*I.m*S.c)/(N.c+epsilon*N.m)^kappa) - d.c*S.c 
+    dI.c<-(beta.cp*S.c*I.c)/N.c^kappa+(epsilon*beta.mp*beta.cp*S.c*I.m)/(N.c+epsilon*N.m)^kappa - I.c*(alpha.c*d.c+d.c+gamma.c)
+    dR.c<-I.c*gamma.c-R.c*d.c
     
-    dS.m<-N.m*(rmax.m+d.m)*(1-N.m/(k.m*phi)) - ((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - d.m*S.m 
-    dI.m<-((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - I.m*(alpha.m*d.p+d.m+gamma.m)
+    dS.m<-N.m*(rmax.m+d.m)*(1-N.m/(k.m*phi)) - ((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.cm*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - d.m*S.m 
+    dI.m<-((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.cm*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - I.m*(alpha.m*d.c+d.m+gamma.m)
     dR.m<-I.m*gamma.m-R.m*d.m
     
-    return(list(c(dS.p, dI.p, dR.p,dS.m, dI.m, dR.m)))
+    return(list(c(dS.c, dI.c, dR.c,dS.m, dI.m, dR.m)))
   })
 }
 
 patch.matrix.model_f <- function(Time, State, Parameters) {
   with(as.list(c(State, Parameters)), {
-    N.p<-S.p+I.p+R.p
+    N.c<-S.c+I.c+R.c
     N.m<-S.m+I.m+R.m
     
-    dS.p<-N.p*(rmax.p+d.p)*(1-N.p*k.p/f) - ((S.p*beta.pp*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*I.m*S.p)/(N.p+epsilon*N.m)^kappa) - d.p*S.p 
-    dI.p<-(beta.pp*S.p*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*S.p*I.m)/(N.p+epsilon*N.m)^kappa - I.p*(alpha.p*d.p+d.p+gamma.p)
-    dR.p<-I.p*gamma.p-R.p*d.p
+    dS.c<-N.c*(rmax.c+d.c)*(1-N.c*k.c/(1.01-phi)) - ((S.c*beta.cp*I.c)/N.c^kappa+(epsilon*psi*beta.m*I.m*S.c)/(N.c+epsilon*N.m)^kappa) - d.c*S.c 
+    dI.c<-(beta.c*S.c*I.c)/N.c^kappa+(epsilon*psi*beta.m*S.c*I.m)/(N.c+epsilon*N.m)^kappa - I.c*(alpha.c*d.c+d.c+gamma.c)
+    dR.c<-I.c*gamma.c-R.c*d.c
     
-    dS.m<-N.m*(rmax.m+d.m)*(1-N.m*k.m/(1.5-f)) - ((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - d.m*S.m 
-    dI.m<-((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - I.m*(alpha.m*d.p+d.m+gamma.m)
+    dS.m<-N.m*(rmax.m+d.m)*(1-N.m*k.m/phi) - ((beta.m*S.m*I.m)/N.m^kappa+(epsilon*psi*beta.c*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - d.m*S.m 
+    dI.m<-((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*psi*beta.c*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - I.m*(alpha.m*d.c+d.m+gamma.m)
     dR.m<-I.m*gamma.m-R.m*d.m
     
-    return(list(c(dS.p, dI.p, dR.p,dS.m, dI.m, dR.m)))
+    return(list(c(dS.c, dI.c, dR.c,dS.m, dI.m, dR.m)))
   })
 }
