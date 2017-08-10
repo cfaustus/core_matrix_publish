6ba966e6245691b57b518816976d995cd21e90c7
c faust macbook pro <christina.faust@gmail.com>
2017-08-10 17:31:44 +0100

    changing patch to core

--- sensitivity/lhc_novaryingR0.R
+++ sensitivity/lhc_novaryingR0.R
@@ -3,10 +3,10 @@ library(tgp)
 # parameter matrix for core-matrix model where humans are in the matrix habitat
 set.seed(101)
 s1 <- lhs(1000,rbind(
-  #ecological characteristics of patch host
+  #ecological characteristics of core host
   c(0,5), #rmax
   c(0.05,0.5),  #d.p
-  c(100,1000), #k.p
+  c(100,10000), #k.p
   #disease characteristics of patch host
   c(0.0192,1), #gamma.p
   c(0,10), #alpha.p
@@ -51,10 +51,10 @@ R0_M = 0.5 # R0 in matrix at f=1.0
 ## calculate beta.pp and beta.mm based on 100% forest cover
 #s1$beta.pp <- R0_C*(s1[['gamma.p']]+s1[['alpha.p']]*s1[['d.p']]+s1[['d.p']])/(1/s1[['k.p']])
 #s1$beta.mm <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])/(0.5/s1[['k.m']])
-s1$beta.pp <- R0_C*(s1[['gamma.p']]+s1[['alpha.p']]*s1[['d.p']]+s1[['d.p']])
+s1$beta.cc <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])
 s1$beta.mm <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])
 
 head(s1)
 
-write.csv(s1,"lhc_humans_fd_no_v_R0.csv", row.names = F)
+write.csv(s1,"lhc_humans_fd_cR01.5_mR00.5.csv", row.names = F)
 
