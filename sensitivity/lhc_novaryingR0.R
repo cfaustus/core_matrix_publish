1991798517cd216770c0bc5ee872664e307a109c
c faust macbook pro <christina.faust@gmail.com>
2017-08-10 17:55:28 +0100

    changing p/c annotations

--- sensitivity/lhc_novaryingR0.R
+++ sensitivity/lhc_novaryingR0.R
@@ -5,11 +5,11 @@ set.seed(101)
 s1 <- lhs(1000,rbind(
   #ecological characteristics of core host
   c(0,5), #rmax
-  c(0.05,0.5),  #d.p
-  c(100,10000), #k.p
+  c(0.05,0.5),  #d.c
+  c(100,10000), #k.c
   #disease characteristics of patch host
-  c(0.0192,1), #gamma.p
-  c(0,10), #alpha.p
+  c(0.0192,1), #gamma.c
+  c(0,10), #alpha.c
   #####################################
   # transmission between patch and matrix
   c(0.01,1), 
@@ -45,16 +45,16 @@ phi=0.5
 s1$phi = phi
 s1$epsilon= (1+cos(phi*(pi*3/2)-2.3))
 s1$kappa=1 # freq dependent
-R0_C = 1.5 # R0 in core at f=1.0
-R0_M = 0.5 # R0 in matrix at f=1.0
+R0_C = 1.5 # R0 in core at phi = 1.0
+R0_M = 0.5 # R0 in matrix at phi = 1.0
 
-## calculate beta.pp and beta.mm based on 100% forest cover
-#s1$beta.pp <- R0_C*(s1[['gamma.p']]+s1[['alpha.p']]*s1[['d.p']]+s1[['d.p']])/(1/s1[['k.p']])
+## calculate beta.cp and beta.mm based on 100% forest cover
+#s1$beta.cp <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])/(1/s1[['k.c']])
 #s1$beta.mm <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])/(0.5/s1[['k.m']])
-s1$beta.cc <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])
-s1$beta.mm <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])
+s1$beta.c <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])
+s1$beta.m <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])
 
 head(s1)
 
-write.csv(s1,"lhc_humans_fd_cR01.5_mR00.5.csv", row.names = F)
+write.csv(s1,"sensitivity/lhc/lhc_humans_fd_cR01.5_mR00.5.csv", row.names = F)
 
