25e9413361f308a5fe852f1415c94fc645c77c21
c faust macbook pro <christina.faust@gmail.com>
2017-08-11 13:46:53 +0100

    fixing parallel code to run

--- sensitivity/parallel_sensitivity_analysis.R
+++ sensitivity/parallel_sensitivity_analysis.R
@@ -1,13 +1,14 @@
 library(snowfall) 
+library(parallel)
 # detecting cores
-#numCores <- detectCores()  
-numCores=8
-sfInit(cpus=numCores,parallel=T)
+ncores <- detectCores()  
+#ncores = 4
+sfInit(cpus=ncores,parallel=T)
 sfSource("sensitivity_func_cluster_phi.R")
 out <- sfClusterApplyLB(param.list,super_ode,times=times)
 out2 <- as.data.frame(t(sapply(out,function(x) x)))
 out3 <- data.frame(matrix(unlist(out2), nrow=1000, byrow=T))
 colnames(out3)<-names(out2)
-write.csv(out3,"out_fd_nochangeinmatrix_13april16.csv",row.names = F)
+write.csv(out3,"output/out_fd_R01.5core_R00.5matrix_11aug17.csv",row.names = F)
 
 sfStop()
