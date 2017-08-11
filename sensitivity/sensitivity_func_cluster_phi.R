915129981ff6ef428151102eea4d7d2f64ca0796
c faust macbook pro <christina.faust@gmail.com>
2017-08-11 13:24:42 +0100

    new models

--- sensitivity/sensitivity_func_cluster_phi.R
+++ sensitivity/sensitivity_func_cluster_phi.R
@@ -1,9 +1,10 @@
 ######### 
-# Function to run ODEs  with varying forest cover
+# Function to run ODEs  
+# with random generation of parameters
 # raina & christina
 ########
 
-s1<- read.csv("lhc_nomatrixv_fd.csv",header=T) # dataframe of random parameters
+s1<- read.csv("lhc/lhc_humans_fd_cR01.5_mR00.5.csv",header=T) # dataframe of random parameters
 param.list <- apply(s1,1,as.list) #converting into a list
 times <- seq(0, 50, by = 0.1) #time steps are constant for each run
 
@@ -11,24 +12,24 @@ times <- seq(0, 50, by = 0.1) #time steps are constant for each run
 super_ode <- function(x,times){
   library(deSolve) #library for ode function
   #x=param.list[[100]]
-  patch.matrix.model <- function(Time, State, Parameters) {
+  core.matrix.model <- function(Time, State, Parameters) {
     with(as.list(c(State, Parameters)), {
-      N.p<-S.p+I.p+R.p
-      N.m<-S.m+I.m+R.m
+      N.c = S.c+I.c+R.c
+      N.m = S.m+I.m+R.m
       
-      dS.p<-N.p*(rmax.p+d.p)*(1-N.p/(k.p*(1.01-phi))) - ((S.p*beta.pp*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*I.m*S.p)/(N.p+epsilon*N.m)^kappa) - d.p*S.p 
-      dI.p<-(beta.pp*S.p*I.p)/N.p^kappa+(epsilon*beta.mp*beta.pp*S.p*I.m)/(N.p+epsilon*N.m)^kappa - I.p*(alpha.p*d.p+d.p+gamma.p)
-      dR.p<-I.p*gamma.p-R.p*d.p
+      dS.c = N.c*(rmax.c+d.c)*(1-N.c/(k.c*(1.01-phi))) - ((S.c*beta.c*I.c)/N.c^kappa+(epsilon*beta.m*psi*I.m*S.c)/(N.c+epsilon*N.m)^kappa) - d.c*S.c 
+      dI.c = (beta.c*S.c*I.c)/N.c^kappa+(epsilon*beta.m*psi*S.c*I.m)/(N.c+epsilon*N.m)^kappa - I.c*(alpha.c*d.c+d.c+gamma.c)
+      dR.c = I.c*gamma.c-R.c*d.c
       
-      dS.m<-N.m*(rmax.m+d.m)*(1-N.m/(k.m*phi)) - ((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - d.m*S.m 
-      dI.m<-((beta.mm*S.m*I.m)/N.m^kappa+(epsilon*beta.mm*beta.pm*S.m*I.p)/(epsilon*N.p+N.m)^kappa) - I.m*(alpha.m*d.p+d.m+gamma.m)
-      dR.m<-I.m*gamma.m-R.m*d.m
+      dS.m = N.m*(rmax.m+d.m)*(1-N.m/(k.m*phi)) - ((beta.m*S.m*I.m)/N.m^kappa+(epsilon*psi*beta.c*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - d.m*S.m 
+      dI.m = ((beta.m*S.m*I.m)/N.m^kappa+(epsilon*psi*beta.c*S.m*I.c)/(epsilon*N.c+N.m)^kappa) - I.m*(alpha.m*d.c+d.m+gamma.m)
+      dR.m = I.m*gamma.m-R.m*d.m
       
-      return(list(c(dS.p, dI.p, dR.p,dS.m, dI.m, dR.m)))
+      return(list(c(S.c, I.c, R.c, S.m, I.m, R.m)))
     })
   }
-  initial.values<-c(S.p=(1.01-x[['phi']])*x[['k.p']],I.p=1,R.p=0,S.m=x[['phi']]*x[['k.m']],I.m=0,R.m=0) 
-  out <- as.data.frame(ode(func=patch.matrix.model,y=initial.values,parms=x,times=times, method = 'ode45'))
+  initial.values = c(S.c=(1.01-x[['phi']])*x[['k.c']],I.c=1,R.c=0,S.m=x[['phi']]*x[['k.m']],I.m=0,R.m=0) 
+  out = as.data.frame(ode(func=core.matrix.model,y=initial.values,parms=x,times=times, method = 'ode45'))
   #return(out)
   max=min(c(nrow(out),length(times)))
   out.vec=c(out[max,2:7],(out[max,3]/(out[max,2]+out[max,3]+out[max,4])),(out[max,6]/(out[max,5]+out[max,6]+out[max,7])),max)
