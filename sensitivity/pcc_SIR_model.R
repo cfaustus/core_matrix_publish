6a5201571b6fc36404422de1729e853798fe0003
c faust macbook pro <christina.faust@gmail.com>
2017-08-11 13:46:16 +0100

    changing p to c

--- sensitivity/pcc_SIR_model.R
+++ sensitivity/pcc_SIR_model.R
@@ -2,23 +2,25 @@
 #
 library(sensitivity)
 #out is the dataframe that the sensitivity_func_cluster outputs
-out<-read.csv("out_13april1_DD.csv", header =T)
-s1<- read.csv("lhc_nomatrixv_dd.csv",header=T) 
-var <- c("rmax.p", 'd.p', 'k.p', 'gamma.p', 'alpha.p', 'beta.pm', 'beta.mp', 'beta.pp','beta.mm')
-s1.var <- s1[ , var]
-s1.demo <- s1.var[,c("rmax.p", 'd.p', 'k.p')]
-ratio<-pcc(X=s1.var,y=(out['I.p']/sum(out[c('I.p','S.p','R.p')]))/(out['I.m']/sum(out[c('I.m','S.m','R.m')])),
+out = read.csv("output/out_fd_R01.5core_R00.5matrix_11aug17.csv", header =T)
+s1 = read.csv("lhc/lhc_humans_fd_cR01.5_mR00.5.csv",header=T) 
+var = c("rmax.c", 'd.c', 'k.c', 'gamma.c', 'alpha.c', 'psi', 
+         "rmax.m", 'd.m', 'k.m', 'gamma.m', 'alpha.m',
+         'beta.c', 'beta.m')
+s1.var = s1[ , var]
+s1.demo = s1.var[,c("rmax.c", 'd.c', 'k.c')]
+ratio = pcc(X=s1.var,y=(out['I.c']/sum(out[c('I.c','S.c','R.c')]))/(out['I.m']/sum(out[c('I.m','S.m','R.m')])),
            rank=T, nboot=1e4,conf=0.95)
-patch<-pcc(X=s1.var,y=(out['I.p']/sum(out[c('I.p','S.p','R.p')])),
+core = pcc(X=s1.var,y=(out['I.c']/sum(out[c('I.c','S.c','R.c')])),
            rank=T, nboot=1e4,conf=0.95)
 
-ratio.inf<-pcc(X=s1.var,y=(out['I.p']/out['I.m']),
+ratio.inf = pcc(X=s1.var,y=(out['I.c']/out['I.m']),
            rank=T, nboot=1e4,conf=0.95)
-patch.inf<-pcc(X=s1.var,y=out['I.p'],
+core.inf = pcc(X=s1.var,y=out['I.c'],
            rank=T, nboot=1e4,conf=0.95)
-matrix.inf <-pcc(X=s1.var,y=(out['I.m']),
+matrix.inf  = pcc(X=s1.var,y=(out['I.m']),
             rank=T, nboot=1e4,conf=0.95) 
-gamma.beta <- pcc(X=s1.demo,y=out['I.m'],
+gamma.beta  =  pcc(X=s1.demo,y=out['I.m'],
                   rank=F, nboot=1e4,conf=0.95)
 # running correlation on number cases vs. prevalence is equivalent 
 
@@ -68,6 +70,6 @@ matplot(out[,"time"],(out[,6]/(out[,5]+out[,6]+out[,7])),type="l",xlab="time",yl
 
 
 
-#x<-pcc(X=s1,y=(out[,3]/rowSums(out[,2:4]))/(out[,6]/rowSums(out[,5:7])),rank=T)
+#x = pcc(X=s1,y=(out[,3]/rowSums(out[,2:4]))/(out[,6]/rowSums(out[,5:7])),rank=T)
 # #this is how to plot the results
 # barplot(as.vector(x$PRCC[[1]]),names.arg = names(s1),cex.names=0.7,las=2,main='Incidences_U/Incidences_D',ylab=expression(rho))
