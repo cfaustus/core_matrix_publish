865880df59d2ef98ae3d8da252484627e3ad046e
cfaust macbook <christina.faust@gmail.com>
2017-09-02 19:01:16 +0100

    new column titles

--- sensitivity/parallel_sensitivity_analysis.R
+++ sensitivity/parallel_sensitivity_analysis.R
@@ -11,9 +11,10 @@ times <- seq(0, 100, by = 0.1) #time steps are constant for each run
 
 out <- sfClusterApplyLB(param.list,super_ode,times=times)
 out2 <- as.data.frame(t(sapply(out,function(x) x)))
-#out3 <- data.frame(matrix(unlist(out2), nrow=1000, byrow=T))
+
+out3 <- data.frame(matrix(unlist(out2), nrow=1000))
 #colnames(out3)<-names(out2)
-colnames(out2) = 
-write.csv(out2,"output/out_fd_R0vary_phi0.1_1sept17.csv",row.names = F)
+colnames(out3) = c('S.c','I.c','R.c','S.m', 'I.m','R.m','p.c','p.m','time','auc.c','auc.m','max.c','max.m')
+write.csv(out3,"output/out_fd_R0vary_phi0.1_1sept17.csv",row.names = F)
 
 sfStop()
