fb8eee854d87a7c93c6c7d6640fa79cea9d16ee9
cfaust macbook <christina.faust@gmail.com>
2017-09-13 18:36:52 +0100

    fixing R0 so it's the right one

--- sensitivity/lhc_varyingR0_annotated.R
+++ sensitivity/lhc_varyingR0_annotated.R
@@ -49,18 +49,18 @@ colnames(s1)<-c(
 s1 <- as.data.frame(s1)
 
 ## FIXED PARAMS
-phi=0.5
+phi=0.9
 s1$phi = phi
 s1$epsilon= (1+cos(phi*(pi*3/2)-2.3))
-s1$kappa=1 # freq dependent
-R0_C = 1.5 # R0 in core at phi = 1.0
-R0_M = 0.5 # R0 in matrix at phi = 1.0
+s1$kappa=0 # density dependent
+# R0_C = 1.5 # R0 in core at phi = 0
+# R0_M = 0.5 # R0 in matrix at phi = 1.0
 
 ## calculate beta.cp and beta.mm based on 100% forest cover
-#s1$beta.c <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])/(1/s1[['k.c']])
-#s1$beta.m <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])/(1/s1[['k.m']]) # is this right?
-s1$beta.c <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])
-s1$beta.m <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])
+s1$beta.c <- s1$R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])/(s1[['k.c']])
+s1$beta.m <- s1$R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])/(s1[['k.m']]) # is this right?
+# s1$beta.c <- R0_C*(s1[['gamma.c']]+s1[['alpha.c']]*s1[['d.c']]+s1[['d.c']])
+# s1$beta.m <- R0_M*(s1[['gamma.m']]+s1[['alpha.m']]*s1[['d.m']]+s1[['d.m']])
 
 s1$b.c = s1$rmax.c + s1$d.c
 s1$b.m = s1$rmax.m + s1$d.m
@@ -71,5 +71,5 @@ s1$alpha2.m = s1$alpha.m*s1$d.m
 
 head(s1)
 
-write.csv(s1,"sensitivity/lhc/lhc_humans_fd_R0vary_phi0.5.csv", row.names = F)
+write.csv(s1,"sensitivity/lhc/lhc_humans_dd_R0vary_phi0.9.csv", row.names = F)
 
