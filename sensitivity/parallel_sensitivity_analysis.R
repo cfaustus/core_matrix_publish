11b960101ef2aaf5aa5a1c9cca3029752e60b230
cfaust macbook <christina.faust@gmail.com>
2017-09-13 18:37:12 +0100

    new output for dd params

--- sensitivity/parallel_sensitivity_analysis.R
+++ sensitivity/parallel_sensitivity_analysis.R
@@ -15,6 +15,6 @@ out2 <- as.data.frame(t(sapply(out,function(x) x)))
 out3 <- data.frame(matrix(unlist(out2), nrow=1000))
 #colnames(out3)<-names(out2)
 colnames(out3) = c('S.c','I.c','R.c','S.m', 'I.m','R.m','p.c','p.m','time','auc.c','auc.m','max.c','max.m')
-write.csv(out3,"output/out_fd_R0vary_phi0.5_13sept17.csv",row.names = F)
+write.csv(out3,"output/out_dd_R0vary_phi0.9_13sept17.csv",row.names = F)
 
 sfStop()
