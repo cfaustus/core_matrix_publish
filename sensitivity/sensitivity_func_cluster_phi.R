1c31f8c08b006bd1b7821d2b76a9f577c7595036
cfaust macbook <christina.faust@gmail.com>
2017-09-13 11:16:04 +0100

    changing time step and parameters to see if that's what's being an obnoxious troll

--- sensitivity/sensitivity_func_cluster_phi.R
+++ sensitivity/sensitivity_func_cluster_phi.R
@@ -5,13 +5,13 @@
 ########
 s1<- read.csv("sensitivity/lhc/lhc_humans_fd_R0vary_phi0.1.csv",header=T) # dataframe of random parameters
 param.list <- apply(s1,1,as.list) #converting into a list
-times <- seq(0, 100, by = 0.1) #time steps are constant for each run
+times <- seq(0, 50, by = 0.001) #time steps are constant for each run
 
 # cluster version of function
 super_ode <- function(x,times){
   library(deSolve) #library for ode function
   library(pracma)
-  x=param.list[[40]]
+  x=param.list[[1000]]
   core.matrix.model <- function(Time, State, Parameters) {
     with(as.list(c(State, Parameters)), {
       N.c = S.c+I.c+R.c
