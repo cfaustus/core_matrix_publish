548158c12c35af34bd0484bf29bdee3e06b0db0b
cfaustus <christina.faust@gmail.com>
2022-07-06 13:31:04 +0100

    fixed indexing

--- deterministic_ngm_calc_varying_withinsp.R
+++ deterministic_ngm_calc_varying_withinsp.R
@@ -13,12 +13,12 @@ library(matrixcalc)
 source('deterministic_ngm_cm_func.R')
 
 # global params
-phi = seq(0.01, 1, by = 0.001) #vector for different forested proportions
-epsilon = (1+cos(phi*(pi*3/2)-2.3))
+phi = seq(0.01, 1, by = 0.001) #vector for different proportion habitat converted 
+epsilon = (1+cos(phi*(pi*3/2)-2.3)) #edge effects
 #epsilon = rep.int(1,length(phi)) # no change in edge effects
-psi = seq(0,1, by=0.05) # aka between
-R0C = 1.5
-R0M = 0.5
+psi = seq(0,1, by=0.05) #  between species scaling factor
+R0C = 1.5 # R0 for core species (i.e. reservoir host)
+R0M = 0.5 # R0 for matrix species (i.e. recipient/spillover host)
 
 ############################################################
 ## DENSITY DEPENDENT R0
@@ -31,38 +31,28 @@ paramsD <- list(d = c(0.1, 0.02),
                 alpha = c(0.001, 0.01),
                 sigma = c(0.05, 0.05))
 
-max_beta_c = (paramsD[['alpha']][1] + paramsD[['gamma']][1] + paramsD[['d']][1])*R0C/paramsD[['k']][1]
-max_beta_m = (paramsD[['alpha']][2] + paramsD[['gamma']][2] + paramsD[['d']][2])*R0M/paramsD[['k']][2]
+beta_c = (paramsD[['alpha']][1] + paramsD[['gamma']][1] + paramsD[['d']][1])*R0C/paramsD[['k']][1]
+beta_m = (paramsD[['alpha']][2] + paramsD[['gamma']][2] + paramsD[['d']][2])*R0M/paramsD[['k']][2]
+beta = c(beta_c, beta_m)
+paramsD[['beta.wDD']] = beta 
+plot(phi, epsilon, col = 'black',
+     ylab = 'relative rates')
 
 
 R0.c.dd = numeric(length(phi)) 
 R0.m.dd = numeric(length(phi)) 
 R0.combin.dd = data.frame(matrix(NA, nrow = length(psi), ncol = length(phi)))
 
-
-beta_c = R0C*(paramsD[['alpha']][1] + paramsD[['gamma']][1] + paramsD[['d']][1])
-beta_m = R0M*(paramsD[['alpha']][2] + paramsD[['gamma']][2] + paramsD[['d']][2])
-beta = list(c(beta_c, beta_m))
-paramsD[['beta.wDD']] = beta 
-plot(phi, epsilon, col = 'black',
-     ylab = 'relative rates')
-#epsilon = (1+cos(phi*(pi*3/2)-2.3))
-
-
-
-
-
 for (j in 1:length(psi)){
   for (i in 1:length(phi)){
-    mat_F <- matrix.F.DD(beta[1][i], paramsD[["beta.wDD"]]*psi[j], epsilon[i], paramsD[['k']], phi[i])
+    mat_F <- matrix.F.DD(paramsD[["beta.wDD"]], paramsD[["beta.wDD"]]*psi[j], epsilon[i], paramsD[['k']], phi[i])
     mat_V <- matrix.V(paramsD[["alpha"]], paramsD[["gamma"]], paramsD[["d"]])
-    
     mat_G <- matrix.inverse(mat_V) %*% mat_F
     eigen.ngm.dd <- eigen(mat_G)
     R0.combin.dd[j,i] = eigen.ngm.dd$values[[1]]
-    R0.c.dd[i] = (beta_c[i]*paramsD[["k"]][1]*(1.01-phi[i]))/
+    R0.c.dd[i] = (paramsD[["beta.wDD"]][1]*paramsD[["k"]][1]*(1.01-phi[i]))/
       (paramsD[["alpha"]][1] + paramsD[["gamma"]][1] + paramsD[["d"]][1])
-    R0.m.dd[i] = (beta_m[i]*paramsD[["k"]][2]*phi[i])/
+    R0.m.dd[i] = (paramsD[["beta.wDD"]][2]*paramsD[["k"]][2]*phi[i])/
       (paramsD[["alpha"]][2] + paramsD[["gamma"]][2] + paramsD[["d"]][2])
   }
 }
@@ -78,13 +68,17 @@ dim(export)
 ############################################################
 ## FREQUENCY DEPENDENT R0
 
+beta_c_fd = R0C*(paramsD[['alpha']][1] + paramsD[['gamma']][1] + paramsD[['d']][1])
+beta_m_fd = R0M*(paramsD[['alpha']][2] + paramsD[['gamma']][2] + paramsD[['d']][2])
+
 # R0; plotting within and between host R0
 paramsF <- list(d = c(0.1, 0.02),
                 k = c(100, 100),
-                #beta.wFD = c(beta_c, beta_m),
+                beta.wFD = c(beta_c_fd, beta_m_fd),
                 gamma = c(0.03, 0.05),
                 alpha = c(0.001, 0.01),
                 sigma = c(0.05, 0.05))
+
 
 R0.m.fd = numeric(length(phi)) #
 R0.c.fd = numeric(length(phi)) #
